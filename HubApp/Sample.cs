4670b8b51664bccf544c2cea21d9a22ad0cad6c6
barkesaid <barke993@gmail.com>
2016-06-27 11:58:16 +0300

    The recipes updated

---
+++ HubApp/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubApp
+{
+    class Sample : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value != this.name)
+                {
+                    name = value;
+                    NotifyPropertyChanged("Name");
+                }
+
+            }
+        }
+
+        private void NotifyPropertyChanged(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
